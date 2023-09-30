7fc6e2be88c1b03be52ae23f78a8a0250388ac8e
s-jad <s_jad@web.de>
2023-09-30 08:05:50 +0200

    Feat: index css imports

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-import './reset.css'
-import './variables.css'
-
-function App() {
-  const appContainer = document.createElement('div');
-  appContainer.classList.add('app-container');
-
-  return appContainer;
-}
+import './reset.css';
+import './variables.css';
+import '../src/styles/app.css';
+import '../src/styles/header.css';
+import '../src/styles/dashboard.css';
+import '../src/styles/sidebar.css';
+import App from './components/app.js';
 
 document.body.appendChild(App());
-
-
-
