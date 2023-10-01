8ced23b31763ad8d3c551eae6454bb8be362d236
s-jad <s_jad@web.de>
2023-10-01 06:21:16 +0200

    Feats: Hamburger button for sidebar menu

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import Header from './header';
 import Dashboard from './dashboard';
 import Sidebar from './sidebar';
+import HamburgerBtn from './hamburger-btn';
 
 export default function App() {
   const appContainer = document.createElement('div');
@@ -9,6 +10,6 @@ export default function App() {
   appContainer.appendChild(Header());
   appContainer.appendChild(Dashboard());
   appContainer.appendChild(Sidebar());
-
+  appContainer.appendChild(HamburgerBtn());
   return appContainer;
 }
