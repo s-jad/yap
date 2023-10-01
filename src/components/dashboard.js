20230e41824c97f9673155284ab96b074584c67b
s-jad <s_jad@web.de>
2023-10-01 06:22:03 +0200

    Feat: img import for gh-pages, pointermove event listener for grid

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,6 +1,84 @@
+import tribe from '../assets/imgs/tribe.svg';
+import joinTribe from '../assets/imgs/join_tribe.svg';
+import gathering from '../assets/imgs/gathering.svg';
+import planGathering from '../assets/imgs/plan_gathering.svg';
+import formTribe from '../assets/imgs/form_tribe.svg';
+import reportIssue from '../assets/imgs/report_issue.svg';
+
+function getDashboardIcons() {
+  const gatheringIcon = new Image();
+  const formTribeIcon = new Image();
+  const joinTribeIcon = new Image();
+  const planGatheringIcon = new Image();
+  const tribeIcon = new Image();
+  const reportIssueIcon = new Image();
+
+  gatheringIcon.src = gathering;
+  formTribeIcon.src = formTribe;
+  joinTribeIcon.src = joinTribe;
+  planGatheringIcon.src = planGathering;
+  tribeIcon.src = tribe;
+  reportIssueIcon.src = reportIssue;
+
+  gatheringIcon.alt = 'A gathering of people';
+  formTribeIcon.alt = 'A person calling people to join them';
+  joinTribeIcon.alt = 'A person joining a group of people';
+  planGatheringIcon.alt = 'A person thinking of a group of people';
+  tribeIcon.alt = 'A group of people';
+  reportIssueIcon.alt = 'A person with an exclamation mark';
+
+  return [
+    gatheringIcon,
+    formTribeIcon,
+    joinTribeIcon,
+    planGatheringIcon,
+    tribeIcon,
+    reportIssueIcon,
+  ];
+}
+
 export default function Dashboard() {
   const dashboardContainer = document.createElement('div');
   dashboardContainer.className = 'dashboard-container';
+  const dashboardGrid = document.createElement('div');
+  dashboardGrid.className = 'dashboard-grid';
+
+  dashboardGrid.innerHTML = `
+    <div class="dashboard-card" tabindex="2">
+      <p class="card-text">Connect with members</p>
+    </div>    
+    <div class="dashboard-card" tabindex="3">
+      <p class="card-text">Form your own tribe</p>
+    </div>    
+    <div class="dashboard-card" tabindex="4">
+      <p class="card-text">Join an existing tribe</p>
+    </div>    
+    <div class="dashboard-card" tabindex="5">
+      <p class="card-text">Plan a gathering</p>
+    </div>    
+    <div class="dashboard-card" tabindex="6">
+      <p class="card-text">Join a gathering</p>
+    </div>    
+    <div class="dashboard-card" tabindex="7">
+      <p class="card-text">Report an issue</p>
+    </div>    
+  `;
+
+  const cards = Array.from(dashboardGrid.querySelectorAll('.dashboard-card'));
+  const texts = Array.from(dashboardGrid.querySelectorAll('.card-text'));
+  const images = getDashboardIcons();
+
+  for (let i = 0; i < cards.length; i += 1) {
+    images[i].className = 'card-icon';
+    cards[i].insertBefore(images[i], texts[i]);
+  }
+
+  dashboardGrid.addEventListener('pointermove', (ev) => {
+    dashboardGrid.style.setProperty('--x', `${ev.x}px`);
+    dashboardGrid.style.setProperty('--y', `${ev.y}px`);
+  });
+
+  dashboardContainer.appendChild(dashboardGrid);
 
   return dashboardContainer;
 }
