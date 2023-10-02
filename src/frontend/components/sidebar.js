16c0bbc3f49073df24696bf8f4052124a3d982d9
s-jad <s_jad@web.de>
2023-10-02 15:11:37 +0200

    Feat: <a> icons and

--- src/frontend/components/sidebar.js
+++ src/frontend/components/sidebar.js
@@ -1,4 +1,23 @@
+import fetchPage from './fetch_apis';
 import logoSVG from '../assets/imgs/logo.svg';
+import homeSVG from '../assets/imgs/home.svg';
+import messagesSVG from '../assets/imgs/envelope.svg';
+
+
+function getIcons() {
+  const home = new Image();
+  home.src = homeSVG;
+  home.alt = 'A house';
+
+  const messages = new Image();
+  messages.src = messagesSVG;
+  messages.alt = 'An envelope';
+
+  return [
+    home,
+    messages,
+  ];
+}
 
 export default function Sidebar() {
   const sidebarContainer = document.createElement('div');
@@ -8,7 +27,27 @@ export default function Sidebar() {
   logo.className = 'logo';
   logo.src = logoSVG;
 
+  const sidebarListFlex = document.createElement('ul');
+  sidebarListFlex.className = 'sidebar-list-flex';
+  sidebarListFlex.innerHTML = `
+    <li class="sidebar-list-item"><a class="sidebar-list-anchor" data-link="/dashboard" href="#"></a></li>
+    <li class="sidebar-list-item"><a class="sidebar-list-anchor" href="#"></a></li>
+  `;
+
+  const icons = getIcons();
+  const listItems = Array.from(sidebarListFlex.querySelectorAll('.sidebar-list-anchor'));
+
+  listItems.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      fetchPage(item.getAttribute('data-link'));
+    });
+
+    icons[index].className = 'sidebar-list-icon';
+    item.appendChild(icons[index]);
+  });
+
   sidebarContainer.appendChild(logo);
+  sidebarContainer.appendChild(sidebarListFlex);
 
   return sidebarContainer;
 }
