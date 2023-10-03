d803a12a71c155195a8efe95ddc493f4c228e6ec
s-jad <s_jad@web.de>
2023-10-03 10:27:43 +0200

    Refactor: simplified app.js, links handled in component files + fetch_api

--- src/frontend/components/app.js
+++ src/frontend/components/app.js
@@ -3,23 +3,15 @@ import Dashboard from './dashboard';
 import Sidebar from './sidebar';
 import HamburgerBtn from './hamburger-btn';
 
-function HomePage() {
-  const homePageContainer = document.createElement('div'); 
-  homePageContainer.id = 'home-page-container';
-  homePageContainer.appendChild(Header());
-  homePageContainer.appendChild(Dashboard());
-  homePageContainer.appendChild(Sidebar());
-  homePageContainer.appendChild(HamburgerBtn());
-
-  return homePageContainer;
-}
-
 export default function App() {
   const appContainer = document.createElement('div');
   appContainer.classList.add('app-container');
   appContainer.id = 'app';
 
-  appContainer.appendChild(HomePage());
+  appContainer.appendChild(Header());
+  appContainer.appendChild(Dashboard());
+  appContainer.appendChild(Sidebar());
+  appContainer.appendChild(HamburgerBtn());
 
   return appContainer;
 }
