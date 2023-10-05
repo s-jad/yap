e0fc98b0fec9cc1cf8d59ad2f957542d0629f82c
s-jad <s_jad@web.de>
2023-10-05 13:31:32 +0200

    Feat: tribe cards generate/filled based on database data

--- src/frontend/components/join-a-tribe.js
+++ src/frontend/components/join-a-tribe.js
@@ -1,90 +1,47 @@
 import '../styles/join-tribe.css';
+import getTribes from './tribes-db-access';
 
-export default function JoinTribe() {
+export default async function JoinTribe() {
   const joinTribeContainer = document.createElement('div');
   joinTribeContainer.id = 'join-tribe-container';
   joinTribeContainer.className = 'removable';
 
+  const tribeGrid = document.createElement('div');
+  tribeGrid.className = 'tribe-grid';
 
-  joinTribeContainer.innerHTML = `
-    <div class="tribe-grid">
+  const tribes = await getTribes();
+  
+  for (let i = 0; i < tribes.length; i += 1) {
+    const tribeUrl = tribes[i].tribe_name
+        .toLowerCase()
+        .replaceAll(' ', '-');
 
-      <a class="tribe-link" href="#" tabindex="1">
+    const tribeCard = document.createElement('a');
+    tribeCard.className = 'tribe-link';
+    tribeCard.innerHTML = `
+      <a class="tribe-link" href="/api/${tribeUrl}" tabindex="${i + 1}">
         <div class="tribe-card">
           <div class="tribe-card-upper-flex">
-            <h2 class="tribe-name">House Music Lovers</h2>
+            <h2 class="tribe-name">${tribes[i].tribe_name}</h2>
             <img class="tribe-icon" alt="Tribal Icon">
           </div>
-          <h3 class="tribe-cta">Dance til you drop</h3>
-          <p class="tribe-description">For those of us who can't get enough chicago house</p>
+          <h3 class="tribe-cta">${tribes[i].tribe_cta}</h3>
+          <p class="tribe-description">${tribes[i].tribe_description}</p>
         </div>
       </a>
+    `;
 
-      <a class="tribe-link" href="#" tabindex="2">
-        <div class="tribe-card">
-          <div class="tribe-card-upper-flex">
-            <h2 class="tribe-name">Animal Collective</h2>
-            <img class="tribe-icon" alt="Tribal Icon">
-          </div>
-          <h3 class="tribe-cta">Can't get enuff fluff!!</h3>
-          <p class="tribe-description">For all of your adorable animal needs, care tips and funny stories</p>
-        </div>
-      </a>
+    tribeGrid.appendChild(tribeCard);
+  };
 
-      <a class="tribe-link" href="#" tabindex="3">
-        <div class="tribe-card">
-          <div class="tribe-card-upper-flex">
-            <h2 class="tribe-name">Politics</h2>
-            <img class="tribe-icon" alt="Tribal Icon">
-          </div>
-          <h3 class="tribe-cta">Well someone has to talk about it</h3>
-          <p class="tribe-description">Current news, international stories and political analysis</p>
-        </div>
-      </a>
-
-      <a class="tribe-link" href="#" tabindex="4">
-        <div class="tribe-card">
-          <div class="tribe-card-upper-flex">
-            <h2 class="tribe-name">Football Family</h2>
-            <img class="tribe-icon" alt="Tribal Icon">
-          </div>
-          <h3 class="tribe-cta">The beautiful game</h3>
-          <p class="tribe-description">For current sports news, stats, match updates and general banter!</p>
-        </div>
-      </a>
-
-      <a class="tribe-link" href="#" tabindex="5">
-        <div class="tribe-card">
-          <div class="tribe-card-upper-flex">
-            <h2 class="tribe-name">The Office</h2>
-            <img class="tribe-icon" alt="Tribal Icon">
-          </div>
-          <h3 class="tribe-cta">Based out of beautiful Scranton, Pennsylvania</h3>
-          <p class="tribe-description">Some office quote I cant remember</p>
-        </div>
-      </a>
-
-      <a class="tribe-link" href="#" tabindex="6">
-        <div class="tribe-card">
-          <div class="tribe-card-upper-flex">
-            <h2 class="tribe-name">Classic Book Club</h2>
-            <img class="tribe-icon" alt="Tribal Icon">
-          </div>
-          <h3 class="tribe-cta">A classic is a book everyone wants to read, but nobody wants to have read</h3>
-          <p class="tribe-description">Discussing antique literature, classics and modern classics</p>
-        </div>
-      </a>
-
-    </div>
-  `;
+  joinTribeContainer.appendChild(tribeGrid);
 
   const tribeCardLinks = Array.from(joinTribeContainer.querySelectorAll('.tribe-link'));
+
 
   tribeCardLinks.forEach((link) => {
     link.addEventListener('click', (ev) => {
       ev.preventDefault();
-      const link = ev.target;
-      console.log('clicked on tribe card => ', link);
     });
   });
 
