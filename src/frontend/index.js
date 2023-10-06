db23aa804c684d728ee3ad220eb9e072add45356
s-jad <s_jad@web.de>
2023-10-06 11:22:07 +0200

    Feat: added css files for new components

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,11 +1,11 @@
 import './reset.css';
 import './variables.css';
+import './styles/general-form-styling.css';
 import './styles/app.css';
 import './styles/header.css';
 import './styles/dashboard.css';
 import './styles/sidebar.css';
 import './styles/hamburger-btn.css';
-import './styles/report-user-form.css';
 import App from './components/app';
 
 const app = await App();
