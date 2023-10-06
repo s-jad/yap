10d0c0ddb510201217de92bca751649fc06ecb49
s-jad <s_jad@web.de>
2023-10-06 11:21:05 +0200

    Refactor: added generic yapp-form-container and yapp-form classes, refactored html

--- src/frontend/components/report-user-form.js
+++ src/frontend/components/report-user-form.js
@@ -1,3 +1,5 @@
+import '../styles/report-user-form.css';
+
 const url = process.env.SERVER_URL;
 
 function addUserBtnEvent(addUserBtn, addUserInput, involvedUserList) {
@@ -13,43 +15,43 @@ function addUserBtnEvent(addUserBtn, addUserInput, involvedUserList) {
 export default function ReportUserIncidentForm() {
   const reportFormContainer = document.createElement('div');
   reportFormContainer.id = 'report-user-issue-form-container';
-  reportFormContainer.className = 'removable';
+  reportFormContainer.className = 'yapp-form-container removable';
 
   reportFormContainer.innerHTML = `
    <h1 class="report-title">Report an Issue</h1>
-   <form action="${url}/report-user-issue" method="POST" class="report-form">
+   <form action="${url}/report-user-issue" method="POST" class="yapp-form">
     <div class="form-border">
-     <label for="r-description">
-       Please provide a detailed description of the incident
-       <textarea class="report-form-item report-description" name="r-description"></textarea>
-     </label>
-      <div class="radio-outer-flex">
-        <p class="r-form-info">Choose the mosts relevant category for the incident</p>
-        <div class="radio-flex">
-          <label for="radio-bullying">
-            Bullying
-            <input id="radio-bullying" type="radio" class="r-form-radio" name="r-type"
-          value="bullying"> 
-          </label>
-          <label for="radio-hacking">
-            Hacking
-            <input id="radio-hacking" type="radio" class="r-form-radio" name="r-type"
-          value="hacking"> 
-          </label>
-          <label for="radio-scam">
-            Scam
-            <input id="radio-scam" type="radio" class="r-form-radio" name="r-type" value="scam"> 
-          </label>
-       </div>
-    </div>
-    <label for="r-select-members">
-      If someone was involded, please add their username
-      <div class="input-btn-flex">
-        <input type="text" id="r-user" name="r-user">
-        <button type="button" class="r-form-btn" id="add-user-btn">Add User</button>
+       <label for="r-description">
+         Please provide a detailed description of the incident
+         <textarea class="report-form-item report-description" name="r-description"></textarea>
+       </label>
+        <div class="radio-outer-flex">
+          <p class="r-form-info">Choose the most relevant category for the incident</p>
+          <div class="radio-flex">
+            <label for="radio-bullying">
+              Bullying
+              <input id="radio-bullying" type="radio" class="r-form-radio" name="r-type"
+            value="bullying"> 
+            </label>
+            <label for="radio-hacking">
+              Hacking
+              <input id="radio-hacking" type="radio" class="r-form-radio" name="r-type"
+            value="hacking"> 
+            </label>
+            <label for="radio-scam">
+              Scam
+              <input id="radio-scam" type="radio" class="r-form-radio" name="r-type" value="scam"> 
+            </label>
+         </div>
       </div>
-      <div id="involved-user-list"></div>
-    </label>
+      <label for="r-select-members">
+        If someone was involved, please add their username
+        <div class="input-btn-flex">
+          <input type="text" id="r-user" name="r-user">
+          <button type="button" class="r-form-btn" id="add-user-btn">Add User</button>
+        </div>
+        <div id="involved-user-list"></div>
+      </label>
     </div>
     <input class="form-submit-input" type="submit" value="Submit report">
    </form>
