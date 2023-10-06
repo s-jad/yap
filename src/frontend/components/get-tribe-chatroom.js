606b50dd7692bd6e2db40dae617b68536c17a8a7
s-jad <s_jad@web.de>
2023-10-06 15:56:51 +0200

    Feats: scrollable message-view, auto focus on input, reset input on msg, timestamps for msg, import css

--- src/frontend/components/get-tribe-chatroom.js
+++ src/frontend/components/get-tribe-chatroom.js
@@ -1,20 +1,38 @@
-function postMessage(message, messageView) {
+import '../styles/general-chatroom-styling.css';
+
+function postMessage(message) {
   if (message === "") {
     return;
   }
 
   const newMessage = document.createElement('div');
-  const timestamp = new Date();
-  newMessage.className = `message-${timestamp}`;
+  const date = new Date().toISOString();
+  const time = date.slice(date.indexOf('T') + 1, date.indexOf('.'))
+  newMessage.className = `message-wrapper message-${date}`;
 
   newMessage.innerHTML = `
-    <div class="message-wrapper">
-      <img src="#" alt="User Icon" class="user-icon"/>
-      <p class="user-message">${message}</p>
-    </div>
+    <img src="" alt="Icon" class="user-icon"/>
+    <p class="user-message">${message}</p>
   `;
+  const timeStamp = document.createElement('div');
+  timeStamp.className = 'timestamp-wrapper';
+  timeStamp.innerHTML = `<p class="timestamp time-${time}">${time}</p>`;
 
+  return {
+    newMessage,
+    timeStamp,
+  };
+}
+
+function handleUserInput(message) {
+  const messageView = document.querySelector('.message-view');
+  const messageTimeline = document.querySelector('.message-timeline');
+
+  const { newMessage, timeStamp } = postMessage(message);
+  console.log("Message => ", newMessage);
+  console.log("timestamp => ", timeStamp);
   messageView.appendChild(newMessage);
+  messageTimeline.appendChild(timeStamp);
 }
 
 export default function TribeChat(tribeName) {
@@ -25,25 +43,39 @@ export default function TribeChat(tribeName) {
   tribeChatContainer.innerHTML = `
     <div class="messages-container-outer">
       <h1 class="chatroom-title">${tribeName}</h1>
-      <div class="messages-container-inner">
-        <div class="message-timeline"></div>
-        <div class="message-view"></div>
+      <div class="messages-scroll-wrapper">
+        <div class="messages-container-inner">
+          <div class="message-timeline"></div>
+          <div class="message-view"></div>
+        </div>
       </div>
-    <footer class="send-message-footer">
-      <div class="send-message-wrapper">
-        <input type="text" class="send-message-input" maxLength="300" placeholder="Your message..."/>
-        <button class="send-message-btn">Send</button>
-      </div>
-    </footer>
+      <footer class="send-message-footer">
+        <div class="send-message-wrapper">
+          <input type="text" class="send-message-input" maxLength="300" placeholder="Your message..."/>
+          <button class="send-message-btn">Send</button>
+        </div>
+      </footer>
     </div>
   `;
 
   const messageInput = tribeChatContainer.querySelector('.send-message-input');
   const messageBtn = tribeChatContainer.querySelector('.send-message-btn');
-  const messageView = tribeChatContainer.querySelector('.message-view');
+  const messagesScrollWrapper = tribeChatContainer.querySelector('.messages-scroll-wrapper');
+
+  messageInput.addEventListener('keypress', (ev) => {
+    if (ev.key === 'Enter') {
+      handleUserInput(messageInput.value);
+      messageInput.value = '';
+      messageInput.focus();
+      messagesScrollWrapper.scrollTop = messagesScrollWrapper.scrollHeight;
+    }
+  });
 
   messageBtn.addEventListener('click', () => {
-    postMessage(messageInput.value, messageView);
+    handleUserInput(messageInput.value);
+    messageInput.value = '';
+    messageInput.focus();
+    messagesScrollWrapper.scrollTop = messagesScrollWrapper.scrollHeight;
   });
 
   return tribeChatContainer;
