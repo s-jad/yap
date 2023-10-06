d1acf26c0805033a6e845242564ad78c15a7e887
s-jad <s_jad@web.de>
2023-10-06 11:16:25 +0200

    MAC function for inserting the new tribe into database

--- src/backend/tribes_mac.cjs
+++ src/backend/tribes_mac.cjs
@@ -13,12 +13,35 @@ function getTribes() {
   });
 }
 
-async function tribesMac(req) {
+function createTribe(newTribeData) {
+  const values = newTribeData;
+  const query = `
+    INSERT into tribes (tribe_name, tribe_cta, tribe_description, formation_date)
+    VALUES (\$1, \$2, \$3, \$4)
+    RETURNING *; 
+  `;
+
+  return new Promise((resolve, reject) => {
+    pg_client.query(query, values, (err, res) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+      } else {
+        resolve(res.rows[0]);
+      }
+    })
+  });
+}
+
+async function tribesMac(req, data) {
   switch (req) {
-    case 'tribes':
+    case 'get-tribes':
       const tribes = await getTribes();
       return tribes;
 
+    case 'create-tribe':
+      const tribe = await createTribe(data);
+      return tribe;
     default:
       break;
   }
