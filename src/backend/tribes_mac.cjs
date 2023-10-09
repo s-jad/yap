22fcfaff6e50d84030e9da8c902a9b7ccff5156f
s-jad <s_jad@web.de>
2023-10-09 10:07:02 +0200

    Feat: message access controller

--- src/backend/tribes_mac.cjs
+++ src/backend/tribes_mac.cjs
@@ -13,6 +13,34 @@ function getTribes() {
   });
 }
 
+function formatTribeUrl(tribeUrl) {
+  let formattedTribeUrl = tribeUrl.replace(/-/g, ' ');
+  formattedTribeUrl = formattedTribeUrl.replace('/', '');
+  formattedTribeUrl = formattedTribeUrl.charAt(0).toUpperCase() + formattedTribeUrl.slice(1);
+
+  return formattedTribeUrl;
+}
+
+function getChatroomMessages(tribeUrl) {
+  const tribe = formatTribeUrl(tribeUrl);
+
+  return new Promise((resolve, reject) => {
+    const query = {
+      text: 'SELECT message_content, message_timestamp FROM messages WHERE tribe_name = \$1',
+      values: [tribe],
+    };
+
+    pg_client.query(query, (err, res) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        resolve(res.rows);
+      }
+    });
+  });
+}
+
 function createTribe(newTribeData) {
   const values = newTribeData;
   const query = `
@@ -42,6 +70,10 @@ async function tribesMac(req, data) {
     case 'create-tribe':
       const tribe = await createTribe(data);
       return tribe;
+
+    case 'get-messages':
+      const messages = await getChatroomMessages(data);
+      return messages;
     default:
       break;
   }
