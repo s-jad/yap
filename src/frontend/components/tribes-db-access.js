ca9b9333806f0c5107626f175686f8038d2f8282
s-jad <s_jad@web.de>
2023-10-09 10:10:37 +0200

    Feat: getMessages fetch function

--- src/frontend/components/tribes-db-access.js
+++ src/frontend/components/tribes-db-access.js
@@ -34,7 +34,27 @@ async function createTribe() {
     });
 }
 
+async function getMessages(tribeUrl) {
+  console.log("getMessages::tribeUrl => ", tribeUrl);
+  return fetch(`/api/get-chatroom-messages?tribeUrl=${encodeURIComponent(tribeUrl)}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.text().then(text => {
+        try {
+          const json = JSON.parse(text);
+          return json;
+        } catch (error) {
+          console.error('createTribe::Error parsing JSON', error);
+          throw new Error('Error parsing JSON');
+        }
+      });
+    });
+}
+
 export {
   getTribes,
   createTribe,
+  getMessages,
 };
