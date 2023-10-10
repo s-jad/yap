7f2882a38f505cdf175913bf7411f7c7abd00a70
s-jad <s_jad@web.de>
2023-10-10 14:18:15 +0200

    Feats: protected routes, authorization, error handling for user authentication

--- server.cjs
+++ server.cjs
@@ -1,15 +1,21 @@
 const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
-const bodyParser = require('body-parser');
 const path = require('path');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
-const app = express();
 const port = process.env.SERVER_PORT;
+const jwtSecret = process.env.JWT_SECRET;
+
 const config = require('./webpack.config.cjs');
 const compiler = webpack(config);
-
+const { authorization } = require('./src/backend/auth.cjs');
 const { tribesMac } = require('./src/backend/tribes_mac.cjs');
+const { comparePwHash } = require('./src/backend/pw_encryption.cjs');
+const { strict } = require('assert');
+
+const app = express();
 
 app.use(
   webpackDevMiddleware(compiler, {
@@ -20,25 +26,74 @@ app.use(
 app.use('/', express.static(path.join(__dirname, '/')));
 app.use('/assets/imgs', express.static(path.join(__dirname, '/assets/imgs')));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api/protected', authorization);
+
+app.post('/api/authenticate-user', async (req, res) => {
+  const username = req.body.user;
+  const password = req.body.pw;
+  try {
+    const passwordHash = await tribesMac('get-password', username);
+
+    try {
+      const authenticated = await comparePwHash(password, passwordHash);
+
+      if (!authenticated) {
+        res.status(401).json({ message: 'Incorrect Password.' })    
+      } else {
+        const token = jwt.sign({ id: username }, jwtSecret, { expiresIn: '3h' });
+        const parts = token.split('.');
+        // Set JWT header and signature in HttpOnly cookie
+        res.cookie('jwt_signature', `${parts[0]}.${parts[1]}`, { 
+          httpOnly: true,
+          sameSite: 'strict',
+        });
+        // Set JWT payload in a JavaScript-accessible cookie
+        res.cookie('jwt_payload', parts[2], { 
+          httpOnly: false,
+          sameSite: 'strict',
+        });
+
+        res.status(200).json({ token });
+      } 
+    } catch (error) {
+      if (error.message === 'Password does not match.') {
+        res.status(401).json({ message: 'Incorrect Password.' });
+      } else {
+        console.error(error);
+        res.status(500).json({ message: 'An error occured.' });
+      }
+    }
+
+  } catch (error) {
+    if (error.message === 'Username or password are incorrect.') {
+      res.status(401).json({ message: 'Incorrect username or password.' });
+    } else {
+      console.error(error);
+      res.status(500).json({ message: 'Database Error.' });
+    }
+  }
+});
 
 // SEND TRIBE_DB DATA TO TO GRID
-app.get('/api/join-a-tribe', async (req, res) => {
+app.get('/api/protected/join-a-tribe', async (req, res) => {
   const tribes = await tribesMac('get-tribes');
+
   res.send(tribes);
 });
 
-app.get('/api/get-chatroom-messages', async (req, res) => {
+app.get('/api/protected/get-chatroom-messages', async (req, res) => {
   const tribeUrl = req.query.tribeUrl;
-  console.log("SERVER::tribeUrl => ", tribeUrl);
-  
   const messages = await tribesMac('get-messages', tribeUrl);
+
   res.send(messages);
 });
 
+
 // CREATE A TRIBE
-app.post('/api/create-a-tribe', async (req, res) => {
+app.post('/api/protected/create-a-tribe', async (req, res) => {
   try {
     const tribe = await tribesMac('create-tribe', req.body);
     res.status(201).json(tribe);
@@ -48,7 +103,7 @@ app.post('/api/create-a-tribe', async (req, res) => {
   }
 });
 
-app.post('/api/report-user-issue', (req, res) => {
+app.post('/api/protected/report-user-issue', (req, res) => {
   console.log(req.body);
   res.send(`form data received ${req.body}`);
 });
