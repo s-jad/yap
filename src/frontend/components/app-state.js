ba00ce2c2a8a97a6580f81cfb82d81713e93a4e3
s-jad <s_jad@web.de>
2023-10-19 11:10:55 +0200

    Feat: showDialog function for short-lived dialog boxes

--- src/frontend/components/app-state.js
+++ src/frontend/components/app-state.js
@@ -8,7 +8,29 @@ function getAppState(key) {
   return appState.get(key);
 }
 
+function showDialog(container, info, category, dialogType) {
+  let dialog = container.querySelector(`.${category}`)
+
+  if (!dialog) {
+    dialog = document.createElement('dialog');
+    dialog.className = `short-lived-dialog ${category} ${dialogType}`;
+    dialog.open = true;
+    dialog.innerHTML = `
+        <p class="dialog-msg">${info}</p>
+    `;
+
+    container.appendChild(dialog);
+  } else {
+    dialog.open = true;
+  }
+  
+  setTimeout(() => {
+    dialog.open = false; 
+  }, 2500);
+}
+
 export {
   updateAppState,
   getAppState,
+  showDialog,
 }
