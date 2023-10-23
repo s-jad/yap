72aaa85a9659662d03efcb6275a6ced455abca93
s-jad <s_jad@web.de>
2023-10-23 12:55:57 +0200

    Feat: main entry point for not logged in users

---
+++ src/frontend/login.js
@@ -0,0 +1,13 @@
+import './reset.css';
+import './variables.css';
+import './styles/general-form-styling.css';
+import './styles/welcome.css';
+import './styles/app.css';
+import './styles/header.css';
+import './styles/dashboard.css';
+import './styles/sidebar.css';
+import './styles/hamburger-btn.css';
+import Welcome from './components/welcome';
+
+const welcomeScreen = Welcome();
+document.body.appendChild(welcomeScreen);
