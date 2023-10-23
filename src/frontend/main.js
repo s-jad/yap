57686af7675b11a4e6db9f6c0b59fd76e0b2ea47
s-jad <s_jad@web.de>
2023-10-23 12:55:32 +0200

    Feat: main entry point for logged in users

---
+++ src/frontend/main.js
@@ -0,0 +1,13 @@
+import './reset.css';
+import './variables.css';
+import './styles/general-form-styling.css';
+import './styles/welcome.css';
+import './styles/app.css';
+import './styles/header.css';
+import './styles/dashboard.css';
+import './styles/sidebar.css';
+import './styles/hamburger-btn.css';
+import App from './components/app';
+
+const app = await App();
+document.body.appendChild(app);
