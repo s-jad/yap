01b4dcfc4ff41f3ece839c00fce6811a09d1dde8
s-jad <s_jad@web.de>
2023-10-27 15:00:51 +0200

    Fix: use 'user disconnect', 'disconnect' not allowed

--- src/frontend/components/sockets.js
+++ src/frontend/components/sockets.js
@@ -4,16 +4,18 @@ let initialized = false;
 let socket;
 
 function initialiseSocket() {
+  console.log("Calling initialiseSocket!!");
   socket = io(process.env.SERVER_URL);
   initialized = true;
   window.addEventListener('beforeunload', () => {
-    socket.emit('disconnect')
+    socket.emit('user disconnect')
   });
 }
 
 function disconnectSocket() {
   initialized = false;
-  socket.emit('disconnect');
+  socket.emit('user disconnect');
+  socket = null;
 }
 
 function getSocketInitState() {
