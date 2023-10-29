01f99e4d4aba3065b63e0c1c92dcba80aff2051f
s-jad <s_jad@web.de>
2023-10-29 09:42:57 +0100

    Feat: added close-modal-btn to modals

--- src/frontend/components/modals.js
+++ src/frontend/components/modals.js
@@ -1,10 +1,27 @@
 import { getTribeMembers } from "./tribes-db-access";
 
+function closeModal(modal) {
+  document.body.removeChild(modal);
+}
+
 function getModal() {
   const modalOuter = document.createElement('div');
   modalOuter.className = 'modal-outer';
   const modalInner = document.createElement('div');
   modalInner.className = 'modal-inner';
+  const btnContainer = document.createElement('div');
+  btnContainer.className = 'modal-btn-container';
+  btnContainer.innerHTML = `
+    <button type="button" class="invisible-btn"></button>
+    <div class="close-modal-btn">X</div>
+  `;
+  
+  const btn = btnContainer.querySelector('button');
+  btn.addEventListener('click', () => {
+    closeModal(modalOuter);
+  }); 
+
+  modalInner.appendChild(btnContainer);
   modalOuter.appendChild(modalInner);
 
   return modalOuter;
@@ -16,6 +33,8 @@ async function getTribeMembersListModal(tribe) {
   const modalInner = modal.querySelector('.modal-inner');
   
   members.forEach((member) => {
+    console.log(`${member.user_name} last logged IN at ${member.last_login}`);
+    console.log(`${member.user_name} last logged OUT at ${member.last_logout}`);
     const memberEl = document.createElement('div');
     memberEl.className = 'member-list-item';
     memberEl.innerHTML = `
