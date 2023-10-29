6ca36b7293b5fbcb7488e6e346e0eec74ba31ce8
s-jad <s_jad@web.de>
2023-10-29 09:44:45 +0100

    Fix: added modal.css to login.js

--- src/frontend/login.js
+++ src/frontend/login.js
@@ -7,6 +7,7 @@ import './styles/header.css';
 import './styles/dashboard.css';
 import './styles/sidebar.css';
 import './styles/hamburger-btn.css';
+import './styles/modal.css';
 import Welcome from './components/welcome';
 
 const welcomeScreen = Welcome();
