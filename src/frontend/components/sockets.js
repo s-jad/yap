f30da6b9b00a01a5f9993431d902997f7f7b0e70
s-jad <s_jad@web.de>
2023-10-29 09:45:21 +0100

    Fix: no error if socket = null, reused disconnectSocket func

--- src/frontend/components/sockets.js
+++ src/frontend/components/sockets.js
@@ -3,19 +3,21 @@ import { io } from "socket.io-client";
 let initialized = false;
 let socket;
 
+function disconnectSocket() {
+  initialized = false;
+  if (socket !== null) {
+    socket.emit('user disconnect');
+    socket = null;
+  }
+}
+
 function initialiseSocket() {
   console.log("Calling initialiseSocket!!");
   socket = io(process.env.SERVER_URL);
   initialized = true;
   window.addEventListener('beforeunload', () => {
-    socket.emit('user disconnect')
+    disconnectSocket();
   });
-}
-
-function disconnectSocket() {
-  initialized = false;
-  socket.emit('user disconnect');
-  socket = null;
 }
 
 function getSocketInitState() {
