f10c758b34d0c7846f7ba60f845da45344e8aef1
s-jad <s_jad@web.de>
2023-11-05 09:03:25 +0100

    Feat: displays list of friends/status/last tribe login

--- src/frontend/components/friends.js
+++ src/frontend/components/friends.js
@@ -1,3 +1,4 @@
+import '../styles/friends.css';
 import { getFriends } from "./tribes-db-access";
 
 export default async function Friends() {
@@ -7,8 +8,46 @@ export default async function Friends() {
   const friendsList = document.createElement('div');
   friendsList.className = 'friends-list';
 
+  const friendsListHead = document.createElement('div');
+  friendsListHead.className = 'friends-list-header';
+
+  friendsListHead.innerHTML = `
+    <h3 class="friends-list-head-item">Friend</h3>
+    <h3 class="friends-list-head-item">Last Tribe Login</h3>
+    <h3 class="friends-list-head-item">Status</h3>
+  `;
+  
+  friendsList.appendChild(friendsListHead);
+
   const friends = await getFriends();
-  console.log("Friends list => ", friends);
+  friends.reverse();
+
+  friends.forEach((friend) => {
+    const friendCard = document.createElement('div');
+    friendCard.className = 'friend-card';
+  
+    const loggedIn = friend.last_login > friend.last_logout;
+
+    let status;
+    let classStatus;
+    if (loggedIn) {
+      status = 'Active';
+      classStatus = 'active';
+    } else {
+      status = 'Logged out';
+      classStatus = 'inactive';
+    }
+
+    friendCard.innerHTML = `
+      <p class="friend-name">${friend.user_name}</p>
+      <p class="friend-last-tribe-login">${friend.tribe_name}</p>
+      <p class="friend-active-status ${classStatus}">${status}</p>
+    `;
+
+    friendsList.appendChild(friendCard);
+  });
+
+  friendsContainer.appendChild(friendsList);
 
   return friendsContainer;
 }
