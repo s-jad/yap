bf8a2c7398d43a8ffc1fa98c58f25593fe5c9e9f
s-jad <s_jad@web.de>
2023-11-06 12:34:55 +0100

    Feat: form now posts reports to pg_db

--- src/frontend/components/report-user-form.js
+++ src/frontend/components/report-user-form.js
@@ -1,16 +1,7 @@
 import '../styles/report-user-form.css';
+import { postUserReport } from './tribes-db-access';
 
 const url = process.env.SERVER_URL;
-
-function addUserBtnEvent(addUserBtn, addUserInput, involvedUserList) {
-
-  addUserBtn.addEventListener('click', () => {
-    const userName = addUserInput.value;
-    involvedUserList.insertAdjacentHTML('beforeend', `<p>${userName}</p>`);
-    addUserInput.value = '';
-    addUserInput.focus();
-  });
-}
 
 export default function ReportUserIncidentForm() {
   const reportFormContainer = document.createElement('div');
@@ -23,45 +14,49 @@ export default function ReportUserIncidentForm() {
     <div class="form-border">
        <label for="r-description">
          Please provide a detailed description of the incident
-         <textarea class="report-form-item report-description" name="r-description"></textarea>
+         <textarea id="r-description" class="report-form-item report-description" name="incidentDescription"></textarea>
        </label>
         <div class="radio-outer-flex">
           <p class="r-form-info">Choose the most relevant category for the incident</p>
           <div class="radio-flex">
             <label for="radio-bullying">
               Bullying
-              <input id="radio-bullying" type="radio" class="r-form-radio" name="r-type"
-            value="bullying"> 
+              <input id="radio-bullying" type="radio" class="r-form-radio" name="incidentType"
+            value="bullying" /> 
             </label>
             <label for="radio-hacking">
               Hacking
-              <input id="radio-hacking" type="radio" class="r-form-radio" name="r-type"
-            value="hacking"> 
+              <input id="radio-hacking" type="radio" class="r-form-radio" name="incidentType"
+            value="hacking" /> 
             </label>
             <label for="radio-scam">
               Scam
-              <input id="radio-scam" type="radio" class="r-form-radio" name="r-type" value="scam"> 
+              <input id="radio-scam" type="radio" class="r-form-radio" name="incidentType"
+            value="scam" /> 
             </label>
          </div>
       </div>
-      <label for="r-select-members">
-        If someone was involved, please add their username
+      <label for="r-users">
+        Please enter the involed members usernames, seperate by a comma
         <div class="input-btn-flex">
-          <input type="text" id="r-user" name="r-user">
-          <button type="button" class="r-form-btn" id="add-user-btn">Add User</button>
+          <input type="text" id="r-users" name="involvedUsers" placeholder="Member1, Member2, Member3..." />
         </div>
-        <div id="involved-user-list"></div>
       </label>
     </div>
     <input class="form-submit-input" type="submit" value="Submit report">
    </form>
   `;
 
-  const addUserBtn = reportFormContainer.querySelector('#add-user-btn');
-  const addUserInput = reportFormContainer.querySelector('#r-user');
-  const involvedUserList = reportFormContainer.querySelector('#involved-user-list');
+  const formSubmitInput = reportFormContainer.querySelector('.form-submit-input');
 
-  addUserBtnEvent(addUserBtn, addUserInput, involvedUserList);
+  formSubmitInput.addEventListener('click', async (ev) => {
+    ev.preventDefault();
+  
+    const formData = new FormData(reportFormContainer.querySelector('.yapp-form'));
+    
+    const result = await postUserReport(formData);
+    console.log("result => ", result);
+  });
 
   return reportFormContainer;
 }
