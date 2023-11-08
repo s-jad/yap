0ea97ed13cb55ef4a8b7e93cc25afbad7bbcdf3a
s-jad <s_jad@web.de>
2023-11-08 12:30:52 +0100

    Feats: added different links for mods/admin, added memberStatus to optional sidebar links

--- src/frontend/components/sidebar.js
+++ src/frontend/components/sidebar.js
@@ -9,28 +9,45 @@ import {
 } from './icons';
 import { getInboxMessageCount } from './tribes-db-access';
 
-function getOptionalSidebarItems(urls) {
-  const optionalItemContainer = document.createElement('ul');
-  optionalItemContainer.className = 'optional-items-container';
+function getOptionalSidebarItems(urls, memberStatus) {
+  const optionalListFlex = document.createElement('ul');
+  optionalListFlex.className = 'optional-list-flex';
 
   if (urls[0] === 'tribe-chat') {
     const tribe = urls[1];
-    const res = setSidebarTribeChat(optionalItemContainer, tribe);
+    const res = setSidebarTribeChat(optionalListFlex, tribe, memberStatus);
     return res;
   } else {
-    return optionalItemContainer;
+    return optionalListFlex;
   }
 }
 
-function setSidebarTribeChat(optionalItemContainer, tribe) {
-  optionalItemContainer.innerHTML = `
-    <li class="sidebar-list-item">
-      <a class="sidebar-list-anchor" data-link="/tribe-chat/${tribe}/members" href="/api/tribe-chat/${tribe}/members"></a>
-    </li>
-  `;
+function setSidebarTribeChat(optionalListFlex, tribe, memberStatus) {
+  if (memberStatus === 'mod' || memberStatus === 'admin') {
+    optionalListFlex.innerHTML = `
+      <li class="sidebar-list-item">
+        <a class="sidebar-list-anchor"
+          data-link="/tribe-chat/${tribe}/members" 
+          href="/api/tribe-chat/${tribe}/members"></a>
+      </li>
+      <li class="sidebar-list-item">
+        <a class="sidebar-list-anchor"
+          data-link="/tribe-chat/${tribe}/applications" 
+          href="/api/tribe-chat/${tribe}/applications"></a>
+      </li>
+    `;
+  } else {
+    optionalListFlex.innerHTML = `
+      <li class="sidebar-list-item">
+      <a class="sidebar-list-anchor"
+        data-link="/tribe-chat/${tribe}/members"
+        href="/api/tribe-chat/${tribe}/members"></a>
+      </li>
+    `;
+  } 
 
-  const links = Array.from(optionalItemContainer.querySelectorAll('a'));
-  const icon = getOptionalSidebarIcons('tribe-chat');
+  const links = Array.from(optionalListFlex.querySelectorAll('a'));
+  const icons = getOptionalSidebarIcons('tribe-chat');
    
   links.forEach((link, index) => {
     link.addEventListener('click', (ev) => {
@@ -39,11 +56,11 @@ function setSidebarTribeChat(optionalItemContainer, tribe) {
       history.pushState(null, null, url);
       handleSidebarOptionalLinks(url);
     });
-    icon.className = 'sidebar-list-icon';
-    link.appendChild(icon);
+    icons[index].className = 'sidebar-list-icon';
+    link.appendChild(icons[index]);
   });
 
-  return optionalItemContainer;
+  return optionalListFlex;
 }
 
 export default async function Sidebar(urls) {
@@ -93,9 +110,10 @@ export default async function Sidebar(urls) {
   sidebarContainer.addEventListener('sidebar-link-change', (ev) => {
     const newUrls = []
     const currentUrlSplit = ev.detail.currentUrl.split('/');
+    const memberStatus = ev.detail.memberStatus;
     newUrls.push(currentUrlSplit[1], currentUrlSplit[2]);
-    const toRemove = sidebarContainer.querySelector('.optional-items-container');
-    const newOptionalListFlex = getOptionalSidebarItems(newUrls);
+    const toRemove = sidebarContainer.querySelector('.optional-list-flex');
+    const newOptionalListFlex = getOptionalSidebarItems(newUrls, memberStatus);
     sidebarContainer.removeChild(toRemove);
     sidebarContainer.appendChild(newOptionalListFlex);
   });
