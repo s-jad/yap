7af0d6d0c3490b9f593877b2ad253ffcfcc4329e
s-jad <s_jad@web.de>
2023-11-11 12:57:00 +0100

    Refactor: removed inboxClient, change to using websockets directly

--- src/backend/redis-client.cjs
+++ src/backend/redis-client.cjs
@@ -7,14 +7,6 @@ const redisChatroomClient = new Redis.createClient({
 
 redisChatroomClient.connect();
 
-const redisInboxClient = new Redis.createClient({
-  host: process.env.DEV_HOST,
-  port: process.env.REDIS_INBOX_PORT,
-});
-
-redisInboxClient.connect();
-
 module.exports = {
   redisChatroomClient,
-  redisInboxClient,
 };
