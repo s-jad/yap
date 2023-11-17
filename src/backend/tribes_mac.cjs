05c4c2edad5f52cc3797887eed228708d15e7b59
s-jad <s_jad@web.de>
2023-11-17 10:38:19 +0100

    Refactor: clustered similar functions together

--- src/backend/tribes_mac.cjs
+++ src/backend/tribes_mac.cjs
@@ -2,6 +2,277 @@ const { pg_client } = require("./tribes_db.cjs");
 const { logger } = require('./logging.cjs');
 const { hashPassword } = require("./pw_encryption.cjs");
 
+async function createUser(newUserData) {
+  const { user, pw, joined, userColor } = newUserData;
+  const pwHash = await hashPassword(pw);
+
+  const query = {
+    text: `
+      INSERT into users (user_name, password, joined, last_login, user_color)
+      VALUES (\$1, \$2, \$3, \$4, \$5)
+      RETURNING *; 
+    `,
+    values: [user, pwHash, joined, joined, userColor],
+  };
+
+  return new Promise((resolve, reject) => {
+    pg_client.query(query, (err, res) => {
+      if (err) {
+        logger.error(err);
+        reject(new Error('Cant create user.'));
+      } else if (res.rows.length === 0) {
+        logger.error(err);
+        reject(new Error('Cant create user.'));
+      } else {
+        const row = res.rows[0];
+        resolve({
+          username: row.user_name,
+          userId: row.user_id,
+          userColor: row.user_color
+        });
+      }
+    })
+  });
+}
+
+function userExists(userData) {
+  const query = `
+    SELECT user_name FROM users WHERE user_id = ${userData};
+  `;
+
+  return new Promise((resolve, reject) => {
+    pg_client.query(query, (err, res) => {
+      if (err) {
+        logger.error(err);
+        reject(new Error('Error checking user exists'));
+      } else if (res.rows.length === 0) {
+        resolve(false);
+      } else {
+        console.log("userExists res.rows[0] => ", res.rows[0]);
+        const user = res.rows[0]
+        resolve(user);
+      }
+    });
+  });
+}
+
+
+function getPwHash(user) {
+  return new Promise((resolve, reject) => {
+    const query = `SELECT user_id, password, user_color FROM users WHERE user_name = '${user}'`;
+
+    pg_client.query(query, (err, res) => {
+      if (err) {
+        logger.error(err);
+        reject(new Error('User with that password does not exist'));
+      } else if (res.rows.length === 0) {
+        reject(new Error('User with that password does not exist'));
+      } else {
+        const row = res.rows[0];
+        resolve({
+          userId: row.user_id,
+          passwordHash: row.password,
+          userColor: row.user_color
+        });
+      }
+    });
+  });
+}
+
+function getFriends(userId) {
+  const query = {
+    text: `
+      SELECT 
+        u.user_name,
+        t.tribe_name,
+        tm.last_login,
+        tm.last_logout
+      FROM users u
+      JOIN friends f ON u.user_id = f.friend_id
+      JOIN tribe_members tm ON u.user_id = tm.member_id
+      JOIN tribes t ON tm.tribe_id = t.tribe_id
+      WHERE (
+        f.user_id = \$1
+        AND tm.last_login = (
+          SELECT MAX(last_login)
+          FROM tribe_members
+          WHERE member_id = u.user_id
+        )
+      )
+      UNION ALL
+      SELECT 
+        u.user_name,
+        t.tribe_name,
+        tm.last_login,
+        tm.last_logout
+      FROM users u
+      JOIN friends f ON u.user_id = f.user_id
+      JOIN tribe_members tm ON u.user_id = tm.member_id
+      JOIN tribes t ON tm.tribe_id = t.tribe_id
+      WHERE (
+        f.friend_id = \$1
+        AND tm.last_login = (
+          SELECT MAX(last_login)
+          FROM tribe_members
+          WHERE member_id = u.user_id
+        )
+      );
+    `,
+    values: [userId],
+  };
+
+  return new Promise((resolve, reject) => {
+    pg_client.query(query, (err, res) => {
+      if (err) {
+        logger.error(err);
+        reject(new Error('Error getting friends list from db'));
+      } else if (res.rows.length === 0) {
+        resolve(false);
+      } else {
+        const friends = res.rows;
+        resolve(friends);
+      }
+    })
+  });
+}
+
+function getRandomTribeSuggestions() {
+  const query = `
+    SELECT 
+      t.tribe_name,
+      ti.tribe_icon
+    FROM tribes t
+    JOIN tribe_icons ti ON t.tribe_id = ti.tribe_id
+    ORDER BY RANDOM()
+    LIMIT 3;
+  `;
+
+  return new Promise((resolve, reject) => {
+    pg_client.query(query, (err, res) => {
+      if (err) {
+        logger.error(err);
+        reject(err)
+      } else {
+        resolve(res.rows);
+      }
+    });
+  });
+}
+
+function getLastTribeLogin(userId) {
+  return new Promise((resolve, reject) => {
+    const query = {
+      text: `
+        SELECT 
+          ti.tribe_icon, 
+          t.tribe_name,
+          tm.last_login
+        FROM 
+          tribe_members tm
+        JOIN tribes t ON tm.tribe_id = t.tribe_id
+        JOIN tribe_icons ti ON t.tribe_id = ti.tribe_id
+        WHERE 
+          tm.member_id = \$1 
+          AND tm.last_login IN (
+            SELECT 
+              last_login 
+            FROM 
+              tribe_members 
+            WHERE 
+              member_id = \$1 
+            ORDER BY 
+              last_login DESC 
+            LIMIT 
+              3
+          )
+        ORDER BY 
+          tm.last_login DESC;
+      `,
+      values: [userId],
+    };
+    
+    pg_client.query(query, (err, res) => {
+      if (err) {
+        logger.error(err);
+        reject(err);
+      } else {
+        if (res.rows.length < 3) {
+          getRandomTribeSuggestions()
+            .then(tribeSuggestions => {
+              resolve(tribeSuggestions);
+            })
+            .catch(err => reject(err));
+        } else {
+          logger.info(res.rows);
+          resolve(res.rows);
+        }
+      }
+    });
+  });
+}
+
+function createTribe(newTribeData) {
+  const { userId, tribeName, tribeCta, tribeDescription, formationDate, icon, tribePrivacy } = newTribeData;
+  console.log("createTribe::newTribeData => ", newTribeData);
+  
+  let query;
+  if (icon === undefined) {
+    query = {
+      text: `
+      INSERT into tribes (
+        founding_member,
+        tribe_name,
+        tribe_cta,
+        tribe_description,
+        formation_date,
+        private
+      )
+      VALUES (\$1, \$2, \$3, \$4, \$5, \$6)
+      RETURNING *; 
+      `,
+      values: [ userId, tribeName, tribeCta, tribeDescription, formationDate, tribePrivacy ],
+    };
+  } else {
+    query = {
+      text: `
+      WITH new_tribe AS (
+        INSERT into tribes (
+          founding_member,
+          tribe_name,
+          tribe_cta,
+          tribe_description,
+          formation_date,
+          private
+        )
+        VALUES (\$1, \$2, \$3, \$4, \$5, \$6)
+        RETURNING tribe_id
+      )
+      INSERT INTO tribe_icons (tribe_id, tribe_icon)
+      SELECT tribe_id, decode(\$7, 'base64') FROM new_tribe
+      RETURNING tribe_id;
+      `,
+      values: [ userId, tribeName, tribeCta, tribeDescription, formationDate, tribePrivacy, icon ],
+    };
+  }
+
+  return new Promise((resolve, reject) => {
+    pg_client.query(query, (err, res) => {
+      if (err) {
+        console.log("actual error");
+        logger.error(err);
+        reject(new Error('Cant create tribe.'));
+      } 
+      else {
+        const newTribeName = tribeName;
+        const tribeId = res.rows[0].tribe_id;
+        const returnData = { newTribeName, tribeId };
+        console.log("new tribeID => ", tribeId);
+        resolve(returnData);
+      }
+    })
+  });
+}
+
 function getTribes() {
   return new Promise((resolve, reject) => {
     const query = `
@@ -115,81 +386,6 @@ function postJoinTribeApplication(applicationData) {
       } else {
         logger.info(res);
         resolve(res.rowCount);
-      }
-    });
-  });
-}
-
-function getLastTribeLogin(userId) {
-  return new Promise((resolve, reject) => {
-    const query = {
-      text: `
-        SELECT 
-          ti.tribe_icon, 
-          t.tribe_name,
-          tm.last_login
-        FROM 
-          tribe_members tm
-        JOIN tribes t ON tm.tribe_id = t.tribe_id
-        JOIN tribe_icons ti ON t.tribe_id = ti.tribe_id
-        WHERE 
-          tm.member_id = \$1 
-          AND tm.last_login IN (
-            SELECT 
-              last_login 
-            FROM 
-              tribe_members 
-            WHERE 
-              member_id = \$1 
-            ORDER BY 
-              last_login DESC 
-            LIMIT 
-              3
-          )
-        ORDER BY 
-          tm.last_login DESC;
-      `,
-      values: [userId],
-    };
-    
-    pg_client.query(query, (err, res) => {
-      if (err) {
-        logger.error(err);
-        reject(err);
-      } else {
-        if (res.rows.length < 3) {
-          getRandomTribeSuggestions()
-            .then(tribeSuggestions => {
-              resolve(tribeSuggestions);
-            })
-            .catch(err => reject(err));
-        } else {
-          logger.info(res.rows);
-          resolve(res.rows);
-        }
-      }
-    });
-  });
-}
-
-function getRandomTribeSuggestions() {
-  const query = `
-    SELECT 
-      t.tribe_name,
-      ti.tribe_icon
-    FROM tribes t
-    JOIN tribe_icons ti ON t.tribe_id = ti.tribe_id
-    ORDER BY RANDOM()
-    LIMIT 3;
-  `;
-
-  return new Promise((resolve, reject) => {
-    pg_client.query(query, (err, res) => {
-      if (err) {
-        logger.error(err);
-        reject(err)
-      } else {
-        resolve(res.rows);
       }
     });
   });
@@ -558,178 +754,6 @@ async function replyToInboxMessage(replyMsgData) {
   }
 }
 
-async function createUser(newUserData) {
-  const { user, pw, joined, userColor } = newUserData;
-  const pwHash = await hashPassword(pw);
-
-  const query = {
-    text: `
-      INSERT into users (user_name, password, joined, last_login, user_color)
-      VALUES (\$1, \$2, \$3, \$4, \$5)
-      RETURNING *; 
-    `,
-    values: [user, pwHash, joined, joined, userColor],
-  };
-
-  return new Promise((resolve, reject) => {
-    pg_client.query(query, (err, res) => {
-      if (err) {
-        logger.error(err);
-        reject(new Error('Cant create user.'));
-      } else if (res.rows.length === 0) {
-        logger.error(err);
-        reject(new Error('Cant create user.'));
-      } else {
-        const row = res.rows[0];
-        resolve({
-          username: row.user_name,
-          userId: row.user_id,
-          userColor: row.user_color
-        });
-      }
-    })
-  });
-}
-
-function userExists(userData) {
-  const query = `
-    SELECT user_name FROM users WHERE user_id = ${userData};
-  `;
-
-  return new Promise((resolve, reject) => {
-    pg_client.query(query, (err, res) => {
-      if (err) {
-        logger.error(err);
-        reject(new Error('Error checking user exists'));
-      } else if (res.rows.length === 0) {
-        resolve(false);
-      } else {
-        console.log("userExists res.rows[0] => ", res.rows[0]);
-        const user = res.rows[0]
-        resolve(user);
-      }
-    });
-  });
-}
-
-function getFriends(userId) {
-  const query = {
-    text: `
-      SELECT 
-        u.user_name,
-        t.tribe_name,
-        tm.last_login,
-        tm.last_logout
-      FROM users u
-      JOIN friends f ON u.user_id = f.friend_id
-      JOIN tribe_members tm ON u.user_id = tm.member_id
-      JOIN tribes t ON tm.tribe_id = t.tribe_id
-      WHERE (
-        f.user_id = \$1
-        AND tm.last_login = (
-          SELECT MAX(last_login)
-          FROM tribe_members
-          WHERE member_id = u.user_id
-        )
-      )
-      UNION ALL
-      SELECT 
-        u.user_name,
-        t.tribe_name,
-        tm.last_login,
-        tm.last_logout
-      FROM users u
-      JOIN friends f ON u.user_id = f.user_id
-      JOIN tribe_members tm ON u.user_id = tm.member_id
-      JOIN tribes t ON tm.tribe_id = t.tribe_id
-      WHERE (
-        f.friend_id = \$1
-        AND tm.last_login = (
-          SELECT MAX(last_login)
-          FROM tribe_members
-          WHERE member_id = u.user_id
-        )
-      );
-    `,
-    values: [userId],
-  };
-
-  return new Promise((resolve, reject) => {
-    pg_client.query(query, (err, res) => {
-      if (err) {
-        logger.error(err);
-        reject(new Error('Error getting friends list from db'));
-      } else if (res.rows.length === 0) {
-        resolve(false);
-      } else {
-        const friends = res.rows;
-        resolve(friends);
-      }
-    })
-  });
-}
-
-function createTribe(newTribeData) {
-  const { userId, tribeName, tribeCta, tribeDescription, formationDate, icon, tribePrivacy } = newTribeData;
-  console.log("createTribe::newTribeData => ", newTribeData);
-  
-  let query;
-  if (icon === undefined) {
-    query = {
-      text: `
-      INSERT into tribes (
-        founding_member,
-        tribe_name,
-        tribe_cta,
-        tribe_description,
-        formation_date,
-        private
-      )
-      VALUES (\$1, \$2, \$3, \$4, \$5, \$6)
-      RETURNING *; 
-      `,
-      values: [ userId, tribeName, tribeCta, tribeDescription, formationDate, tribePrivacy ],
-    };
-  } else {
-    query = {
-      text: `
-      WITH new_tribe AS (
-        INSERT into tribes (
-          founding_member,
-          tribe_name,
-          tribe_cta,
-          tribe_description,
-          formation_date,
-          private
-        )
-        VALUES (\$1, \$2, \$3, \$4, \$5, \$6)
-        RETURNING tribe_id
-      )
-      INSERT INTO tribe_icons (tribe_id, tribe_icon)
-      SELECT tribe_id, decode(\$7, 'base64') FROM new_tribe
-      RETURNING tribe_id;
-      `,
-      values: [ userId, tribeName, tribeCta, tribeDescription, formationDate, tribePrivacy, icon ],
-    };
-  }
-
-  return new Promise((resolve, reject) => {
-    pg_client.query(query, (err, res) => {
-      if (err) {
-        console.log("actual error");
-        logger.error(err);
-        reject(new Error('Cant create tribe.'));
-      } 
-      else {
-        const newTribeName = tribeName;
-        const tribeId = res.rows[0].tribe_id;
-        const returnData = { newTribeName, tribeId };
-        console.log("new tribeID => ", tribeId);
-        resolve(returnData);
-      }
-    })
-  });
-}
 
 function addNewTribeMember(newMemberData) {
   const { userId, tribeId, memberRole } = newMemberData;
@@ -750,28 +774,6 @@ function addNewTribeMember(newMemberData) {
       } else {
         logger.info(res);
         resolve(res);
-      }
-    });
-  });
-}
-
-function getPwHash(user) {
-  return new Promise((resolve, reject) => {
-    const query = `SELECT user_id, password, user_color FROM users WHERE user_name = '${user}'`;
-
-    pg_client.query(query, (err, res) => {
-      if (err) {
-        logger.error(err);
-        reject(new Error('User with that password does not exist'));
-      } else if (res.rows.length === 0) {
-        reject(new Error('User with that password does not exist'));
-      } else {
-        const row = res.rows[0];
-        resolve({
-          userId: row.user_id,
-          passwordHash: row.password,
-          userColor: row.user_color
-        });
       }
     });
   });
@@ -836,6 +838,35 @@ function updateTribeMemberLogin(loginData) {
   });
 }
 
+function updateTribeMemberLogout(logoutData) {
+  const { timestamp, tribe, member } = logoutData;
+  return new Promise((resolve, reject) => {
+    const query = {
+      text: `
+        UPDATE tribe_members
+        SET last_logout = \$1
+        WHERE (
+          tribe_id = (
+            SELECT tribe_id FROM tribes WHERE tribe_name = \$2
+          )
+          AND member_id = \$3
+        )
+        RETURNING *;
+      `,
+      values: [timestamp, tribe, member],
+    };
+
+    pg_client.query(query, (err, res) => {
+      if (err) {
+        logger.error(err);
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    })
+  });
+}
+
 function postUserIncidentReport(incidentData) {
   const {
     userId,
@@ -872,34 +903,6 @@ function postUserIncidentReport(incidentData) {
   });
 }
 
-function updateTribeMemberLogout(logoutData) {
-  const { timestamp, tribe, member } = logoutData;
-  return new Promise((resolve, reject) => {
-    const query = {
-      text: `
-        UPDATE tribe_members
-        SET last_logout = \$1
-        WHERE (
-          tribe_id = (
-            SELECT tribe_id FROM tribes WHERE tribe_name = \$2
-          )
-          AND member_id = \$3
-        )
-        RETURNING *;
-      `,
-      values: [timestamp, tribe, member],
-    };
-
-    pg_client.query(query, (err, res) => {
-      if (err) {
-        logger.error(err);
-        reject(err);
-      } else {
-        resolve(res);
-      }
-    })
-  });
-}
 
 async function tribesMac(req, data) {
   switch (req) {
