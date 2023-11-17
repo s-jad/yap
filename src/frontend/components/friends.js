099e4a21bcc60d586e56614f664e0627fc532472
s-jad <s_jad@web.de>
2023-11-17 12:01:58 +0100

    Feat: added user app activity status to friends list for each friend

--- src/frontend/components/friends.js
+++ src/frontend/components/friends.js
@@ -2,6 +2,44 @@ import '../styles/friends.css';
 import { getFriendsCardOptionsModal } from './modals';
 import { getFriends } from "./tribes-db-access";
 
+function checkUserActive(lastLogin, lastLogout) {
+    const loggedIn = lastLogin > lastLogout;
+
+    let userStatus;
+    let userClassStatus;
+    if (loggedIn) {
+      userStatus = 'Active';
+      userClassStatus = 'active';
+    } else {
+      userStatus = 'Logged out';
+      userClassStatus = 'inactive';
+    }
+
+  return {
+    userStatus,
+    userClassStatus,
+  };
+}
+
+function checkUserTribeActivity(lastLogin, lastLogout) {
+    const loggedIn = lastLogin > lastLogout;
+
+    let tribeStatus;
+    let tribeClassStatus;
+    if (loggedIn) {
+      tribeStatus = 'In chat';
+      tribeClassStatus = 'active';
+    } else {
+      tribeStatus = 'Not in chat';
+      tribeClassStatus = 'inactive';
+    }
+
+  return {
+    tribeStatus,
+    tribeClassStatus,
+  };
+}
+
 export default async function Friends() {
   const friendsContainer = document.createElement('div');
   friendsContainer.className = 'friends-container removable';
@@ -14,8 +52,9 @@ export default async function Friends() {
   friendsList.innerHTML = `
     <div class="friends-list-header">
       <h3 class="friends-list-head-item">Friend</h3>
+      <h3 class="friends-list-head-item">Login</h3>
       <h3 class="friends-list-head-item">Last Tribe Login</h3>
-      <h3 class="friends-list-head-item">Status</h3>
+      <h3 class="friends-list-head-item">Tribe Login Status</h3>
     </div>
   `;
 
@@ -30,23 +69,23 @@ export default async function Friends() {
   friends.forEach((friend) => {
     const friendCard = document.createElement('div');
     friendCard.className = 'friend-card';
+    console.log("friend => ", friend)
 
-    const loggedIn = friend.last_login > friend.last_logout;
+    const {
+      userStatus,
+      userClassStatus,
+    } = checkUserActive(friend.last_login, friend.last_logout);
 
-    let status;
-    let classStatus;
-    if (loggedIn) {
-      status = 'Active';
-      classStatus = 'active';
-    } else {
-      status = 'Logged out';
-      classStatus = 'inactive';
-    }
+    const {
+      tribeStatus,
+      tribeClassStatus,
+    } = checkUserTribeActivity(friend.last_tribe_login, friend.last_tribe_logout);
 
     friendCard.innerHTML = `
       <p class="friend-name">${friend.user_name}</p>
+      <p class="friend-active-status ${userClassStatus}">${userStatus}</p>
       <p class="friend-last-tribe-login">${friend.tribe_name}</p>
-      <p class="friend-active-status ${classStatus}">${status}</p>
+      <p class="friend-tribe-active-status ${tribeClassStatus}">${tribeStatus}</p>
     `;
 
     friendCard.addEventListener('click', () => {
