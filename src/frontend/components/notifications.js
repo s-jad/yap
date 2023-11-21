ef2de1af933be5f95427a0cb1c3da421a2a84ac4
s-jad <s_jad@web.de>
2023-11-21 14:44:43 +0100

    Feat: filter notifications by type

--- src/frontend/components/notifications.js
+++ src/frontend/components/notifications.js
@@ -9,10 +9,10 @@ export default async function Notifications() {
     <div class="notifications-outer">
       <div class="filter-bar">
         <ul class="filter-list">
-          <li class="filter-list-option displayed">All</li>
-          <li class="filter-list-option">Friends</li>
-          <li class="filter-list-option">Tribes</li>
-          <li class="filter-list-option">Yapp</li>
+          <li class="filter-list-option displayed" data-filter="all">All</li>
+          <li class="filter-list-option" data-filter="friends">Friends</li>
+          <li class="filter-list-option" data-filter="tribe">Tribes</li>
+          <li class="filter-list-option" data-filter="yapp">Yapp</li>
       </ul>
       </div>
       <div class="notifications-scroll-wrapper">
@@ -31,11 +31,46 @@ export default async function Notifications() {
 
     nCard.innerHTML = `
       <h3 class="notification-sender">${n.notification_sender}</h3>
-      <div class="notification-msg">${n.notification_content}</div>
+      <div class="notification-content">${n.notification_content}</div>
       <div class="notification-timestamp">${n.notification_timestamp}</div>
     `;
 
     nInner.appendChild(nCard);
+  });
+
+  const filterOptions = Array.from(nContainer.querySelectorAll('.filter-list-option'));
+
+  filterOptions.forEach((option) => {
+    option.addEventListener('click', () => {
+      if (!option.classList.contains('displayed')) {
+        const currentlyDisplayed = nContainer.querySelector('.displayed');
+        currentlyDisplayed.classList.remove('displayed');
+        option.classList.add('displayed');
+
+        const dataFilter = option.getAttribute('data-filter');
+
+        const displayedCards = Array.from(nContainer.querySelectorAll('.notification-card'));
+        
+        displayedCards.forEach((card) => {
+          if (dataFilter === 'all') {
+            card.classList.remove('hidden');
+            return;
+          }
+
+          if (
+            card.classList.contains(dataFilter)
+            && card.classList.contains('hidden')
+          ) {
+            card.classList.remove('hidden');
+          } else if (
+            !card.classList.contains(dataFilter)
+            && !card.classList.contains('hidden')
+          ) {
+            card.classList.add('hidden');
+          }
+        });
+      }
+    });
   });
 
   return nContainer;
