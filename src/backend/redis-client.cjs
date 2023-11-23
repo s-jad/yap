f3d3d011d1b516c1f2a1671a0e372e960ccb1f0b
s-jad <s_jad@web.de>
2023-11-23 13:10:06 +0100

    Feat: redis caching of tribes info on server load

--- src/backend/redis-client.cjs
+++ src/backend/redis-client.cjs
@@ -1,4 +1,6 @@
 const Redis = require('redis');
+const { logger } = require('./logging.cjs');
+const { tribesMac } = require('./tribes_mac.cjs');
 
 const redisChatroomClient = new Redis.createClient({
   host: process.env.DEV_HOST,
@@ -6,15 +8,42 @@ const redisChatroomClient = new Redis.createClient({
 });
 
 
-const redisNotificationsClient = new Redis.createClient({
+const redisGeneralClient = new Redis.createClient({
   host: process.env.DEV_HOST,
-  port: process.env.REDIS_NOTIFICATIONS_PORT,
+  port: process.env.REDIS_GENERAL_PORT,
 });
 
 redisChatroomClient.connect();
-redisNotificationsClient.connect();
+redisGeneralClient.connect();
+
+async function cacheTribes() {
+  logger.info('redisGeneralClient => preloading tribes');
+
+  const exists = await redisGeneralClient.exists('tribes');
+
+  if (exists === 1) {
+    logger.info('redisGeneralClient => tribes list already exists')
+    return;
+  } else {
+    logger.info('redisGeneralClient => creating tribes list');
+    const tribes = await tribesMac('get-tribes');
+    
+    for (const tribe of tribes) {
+      const tribeString = JSON.stringify(tribe);
+      try {
+        await redisGeneralClient.lPush('tribes', tribeString);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+}
+
+setTimeout(() => {
+  cacheTribes();
+}, 2000);
 
 module.exports = {
   redisChatroomClient,
-  redisNotificationsClient,
+  redisGeneralClient,
 };
