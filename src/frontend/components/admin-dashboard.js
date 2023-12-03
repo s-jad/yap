4388016eb8a7a198f4a9ca54ce5fd624479cf105
s-jad <s_jad@web.de>
2023-12-03 10:27:21 +0100

    Feat: get user activity charts per user

--- src/frontend/components/admin-dashboard.js
+++ src/frontend/components/admin-dashboard.js
@@ -2,6 +2,76 @@ import '../styles/admin-dashboard.css';
 import { getMonthlyLoginStats, getUserActivityStats } from './tribes-db-access';
 import * as d3 from 'd3';
 
+function clearPreviousChart(container) {
+  const svg = container.querySelector('svg');
+
+  if (svg !== undefined) {
+    container.removeChild(svg);
+  }
+}
+
+function renderUserActivityChartByUser(userActivityData, userName) {
+  const userActivtyContainer = document.body.querySelector('.stats-container.user-activity');
+  clearPreviousChart(userActivtyContainer);
+
+  const rect = userActivtyContainer.getBoundingClientRect();
+  const keysArr = Object.keys(userActivityData);
+  const valuesArr = Object.values(userActivityData);
+  console.log("keysArr => ", keysArr);
+  const yMax = Math.max(...valuesArr);
+
+  const margin = {top: 90, right: 30, bottom: 30, left: 30},
+    height = rect.height - margin.top - margin.bottom,
+    width = rect.width - margin.left - margin.right;
+  
+  console.log("height => ", height);
+
+  const svg = d3.select(userActivtyContainer)
+    .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+      .attr('transform',
+            'translate(' + margin.left + ',' + margin.top + ')');
+
+  const xScale = d3.scaleBand()
+    .range([0, width])
+    .padding(0.2)
+    .domain(keysArr);
+
+  const yScale = d3.scaleLinear()
+    .range([height, 0])
+    .domain([0, yMax]);
+
+   svg.append('text')
+     .attr('x', width / 5)
+     .attr('y', -20)
+     .attr('text-anchor', 'middle')
+     .style('font-size', '1.6rem')
+     .style('fill', '#dec4e3')
+     .text(userName); 
+
+  svg.append('g')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(d3.axisBottom(xScale))
+    .selectAll('text')
+      .attr('transform', 'translate(-10,0)')
+      .style('text-anchor', 'center');
+
+  svg.append('g')
+    .call(d3.axisLeft(yScale));
+
+  svg.selectAll('.bar')
+    .data(valuesArr)
+    .enter()
+    .append('rect')
+      .attr('x', function(d, i) { return  xScale(keysArr[i]) })
+      .attr('y', function (d) { return yScale(d) })
+      .attr('width', xScale.bandwidth())
+      .attr('height', function(d) { return height - yScale(d); })
+      .attr('fill', '#dec4e3')
+}
+
 function renderUserActivityChartByCategory(userActivityData, category) {
   const userActivtyContainer = document.body.querySelector('.stats-container.user-activity');
   const rect = userActivtyContainer.getBoundingClientRect();
@@ -64,7 +134,7 @@ function filterProps(arr, propsToExclude) {
 }
 
 function filterUser(arr, userToInclude) {
-  return arr.filter((user) => {
+  return arr.find((user) => {
     return user.user_name === userToInclude;
   });
 }
@@ -75,7 +145,10 @@ function filterUserActivityInterface(userActivityData, filterType, filterScheme)
     case 'user':
       filteredData = filterUser(userActivityData, filterScheme);
       console.log("filteredData => ", filteredData);
-      renderUserActivityChartByUser(filteredData);
+      const userName = filteredData.user_name;
+      delete filteredData.user_name;
+      delete filteredData.user_id;
+      renderUserActivityChartByUser(filteredData, userName);
       break;
 
     case 'category':
